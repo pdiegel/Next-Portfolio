19311cdfa3c7a0775b74d83de770f03f713305e9
Philip Diegel <philipdiegel@gmail.com>
2024-02-20 09:39:28 -0500

    Added Vercel Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import '@/styles/globals.css'
 import Footer from '@/components/footer';
 import Header from '@/components/header';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
     return (
         <div className='container'>
             <Header />
             <Component {...pageProps} />
+            <Analytics />
             <Footer />
         </div>
     );
